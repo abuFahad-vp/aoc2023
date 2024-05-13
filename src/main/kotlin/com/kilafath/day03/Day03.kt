5f417ff4283766e20236b42e21a66f7e65eec87c
abuFahad-vp <fahadabu30c@gmail.com>
2024-05-13 08:15:56 +0530

    refactoring.

--- src/main/kotlin/com/kilafath/day03/Day03.kt
+++ src/main/kotlin/com/kilafath/day03/Day03.kt
@@ -16,32 +16,32 @@ class Day03 {
         val symbolPattern = Regex("""[^\d.]""")
         val gearPatten = Regex("""\*""")
         val ans = this.foldIndexed(Pair(0,0)) { index,acc, line ->
-        val part1 = acc.first + numPattern.findAll(line).fold(0) {accInner, elem ->
-            for(i in (if (index > 0) index-1 else index)..(if (index < this.size-1) index+1 else index)) {
-                val matchedSymbols = symbolPattern.findAll(this[i])
-                if(isPartNumber(matchedSymbols,elem.range)) {
-                    return@fold accInner + elem.value.toInt()
-                }
-            }
-            accInner
-        }
-
-        val part2 = acc.second + gearPatten.findAll(line).fold(0) { accInner, gear ->
-                var count = 0
-                var values = 1
+            val part1 = acc.first + numPattern.findAll(line).fold(0) {accInner, elem ->
                 for(i in (if (index > 0) index-1 else index)..(if (index < this.size-1) index+1 else index)) {
-                    val matchedSymbols = numPattern.findAll(this[i])
-                    for(elem in matchedSymbols) {
-                        if (gear.range.first >= elem.range.first - 1 && gear.range.first <= elem.range.last+1) {
-                            count++
-                            values *= elem.value.toInt()
-                        }
+                    val matchedSymbols = symbolPattern.findAll(this[i])
+                    if(isPartNumber(matchedSymbols,elem.range)) {
+                        return@fold accInner + elem.value.toInt()
                     }
                 }
-                if (count == 2) accInner + values else accInner
+                accInner
             }
-            Pair(part1, part2)
-        }
+
+            val part2 = acc.second + gearPatten.findAll(line).fold(0) { accInner, gear ->
+                    var count = 0
+                    var values = 1
+                    for(i in (if (index > 0) index-1 else index)..(if (index < this.size-1) index+1 else index)) {
+                        val matchedSymbols = numPattern.findAll(this[i])
+                        for(elem in matchedSymbols) {
+                            if (gear.range.first >= elem.range.first - 1 && gear.range.first <= elem.range.last+1) {
+                                count++
+                                values *= elem.value.toInt()
+                            }
+                        }
+                    }
+                    if (count == 2) accInner + values else accInner
+                }
+                Pair(part1, part2)
+            }
         println("part 1 = ${ans.first}, part 2 = ${ans.second}")
     }
 }
