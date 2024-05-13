b00715325ed72a5033fad07a57cc6ab34cfda348
abuFahad-vp <fahadabu30c@gmail.com>
2024-05-13 08:13:42 +0530

    day 3 part 2 completed.

--- src/main/kotlin/com/kilafath/day03/Day03.kt
+++ src/main/kotlin/com/kilafath/day03/Day03.kt
@@ -4,26 +4,44 @@ import com.kilafath.*
 class Day03 {
     init {
         readAsLines("day03/input.txt").solve()
+
+    }
+
+    private fun isPartNumber(matchedSymbols: Sequence<MatchResult>, range: IntRange): Boolean {
+        return matchedSymbols.any { it.range.first in (range.first - 1)..(range.last + 1) }
     }
 
     private fun List<String>.solve() {
         val numPattern = Regex("""\d+""")
         val symbolPattern = Regex("""[^\d.]""")
-        val ans = this.foldIndexed(0) { index,acc, line ->
-            acc + numPattern.findAll(line).fold(0) {accInner, elem ->
+        val gearPatten = Regex("""\*""")
+        val ans = this.foldIndexed(Pair(0,0)) { index,acc, line ->
+        val part1 = acc.first + numPattern.findAll(line).fold(0) {accInner, elem ->
+            for(i in (if (index > 0) index-1 else index)..(if (index < this.size-1) index+1 else index)) {
+                val matchedSymbols = symbolPattern.findAll(this[i])
+                if(isPartNumber(matchedSymbols,elem.range)) {
+                    return@fold accInner + elem.value.toInt()
+                }
+            }
+            accInner
+        }
+
+        val part2 = acc.second + gearPatten.findAll(line).fold(0) { accInner, gear ->
+                var count = 0
+                var values = 1
                 for(i in (if (index > 0) index-1 else index)..(if (index < this.size-1) index+1 else index)) {
-                    val matchedSymbols = symbolPattern.findAll(this[i])
-                    if(isPartNumber(matchedSymbols,elem.range)) {
-                        return@fold accInner + elem.value.toInt()
+                    val matchedSymbols = numPattern.findAll(this[i])
+                    for(elem in matchedSymbols) {
+                        if (gear.range.first >= elem.range.first - 1 && gear.range.first <= elem.range.last+1) {
+                            count++
+                            values *= elem.value.toInt()
+                        }
                     }
                 }
-                accInner
+                if (count == 2) accInner + values else accInner
             }
+            Pair(part1, part2)
         }
-        println(ans)
-    }
-
-    private fun isPartNumber(matchedSymbols: Sequence<MatchResult>, range: IntRange): Boolean {
-        return matchedSymbols.any { it.range.first in (range.first - 1)..(range.last + 1) }
+        println("part 1 = ${ans.first}, part 2 = ${ans.second}")
     }
 }
